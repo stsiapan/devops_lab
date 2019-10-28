cb62fad58019b3fb79bd6729f161da99a1646078
stsiapan <55788184+stsiapan@users.noreply.github.com>
2019-10-28 11:40:59 +0300

    homework3_v4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="snapshot",
     packages=find_packages(),
-    scripts=[],
+    scripts=["my_project.py"],
     version="0.1",
     author="Stsiapan Hanchar",
     author_email="Stsiapan_Hanchar@epam.com",
